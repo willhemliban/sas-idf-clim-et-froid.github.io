c18cbb3cc0bce1c03fd2ab1edc6634c9fefdae41
willhem <libanwillhem@gmail.com>
2023-01-17 15:18:01 +0100

    new index in php

---
+++ index.php
@@ -0,0 +1,432 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>SAS IDF Froid et Climatisation</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Gp - v4.9.1
+  * Template URL: https://bootstrapmade.com/gp-free-multipurpose-html-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top ">
+    <div class="container d-flex align-items-center justify-content-lg-between">
+
+      <h1 class="logo me-auto me-lg-0"><a href="index.html">SAS IDF Froid et Climatisation<span>.</span></a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Accueil</a></li>
+          <li><a class="nav-link scrollto" href="#about">A propos</a></li>
+          <li><a class="nav-link scrollto " href="#portfolio">Photos</a></li>
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center justify-content-center">
+    <div class="container" data-aos="fade-up">
+
+      <div class="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
+        <div class="col-xl-6 col-lg-8">
+          <h1>SAS IDF Froid et Climatisation<span>.</span></h1>
+          <h2>Installation - Dépannage - Maintenance</h2>
+        </div>
+      </div>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row">
+          <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
+            <img src="assets/img/about.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right" data-aos-delay="100">
+            <h3>Professionnels de la climatisation disponibles dans toute l'Ile-de-France</h3>
+            <p class="fst-italic">
+              Prestation de qualité à des tarifs compétitifs
+            </p>
+            <ul>
+              <li><i class="ri-check-double-line"></i>Étude, Installation et Maintenance</li>
+              <li><i class="ri-check-double-line"></i> Pour professionnels et particuliers.</li>
+              <li><i class="ri-check-double-line"></i>Pour tout vos besoins de climatisation.</li>
+              <li><i class="ri-check-double-line"></i> Professionnels à votre écoute.</li>
+            </ul>
+            <p>
+              Une équipe experimentée pour vos besoins de climatisation. Des équipements dernier cris avec de nombreuses marques telles que : LG, Mitsubishi Electric, Daikin et tant d'autres... Et un accompagnement personnalisé que vous soyez Professionnels ou particuliers
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Brands Section ======= -->
+    <section id="brands" class="brands">
+      <div class="container" data-aos="zoom-in">
+
+        <div class="clients-slider swiper">
+          <div class="swiper-wrapper align-items-center">
+            <div class="swiper-slide"><img src="assets/img/brands/airwell.png" class="img-fluid" alt=""></div>
+            <div class="swiper-slide"><img src="assets/img/brands/LG.png" class="img-fluid" alt=""></div>
+            <div class="swiper-slide"><img src="assets/img/brands/Daikin-logo.png" class="img-fluid" alt=""></div>
+            <div class="swiper-slide"><img src="assets/img/brands/Mitsubishi_Electric_logo.png" class="img-fluid" alt=""></div>
+            <div class="swiper-slide"><img src="assets/img/brands/toshiba.png" class="img-fluid" alt=""></div>
+            <div class="swiper-slide"><img src="assets/img/brands/Hitachi-Logo.png" class="img-fluid" alt=""></div>
+            <div class="swiper-slide"><img src="assets/img/brands/samsung.png" class="img-fluid" alt=""></div>
+            
+          </div>
+          <div class="swiper-pagination"></div>
+        </div>
+
+      </div>
+    </section><!-- End Clients Section -->
+
+    <!-- ======= Cta Section ======= -->
+    <section id="cta" class="cta">
+      <div class="container" data-aos="zoom-in">
+
+        <div class="text-center">
+          <h3>Devis gratuit</h3>
+          <p> Prenez rendez-vous par téléphone au 01 74 34 44 72 ou par mail en cliquant ci-dessous </p>
+          <a class="cta-btn" href="mailto:clim.froid.idf@gmail.com">Demandez un devis!</a>
+        </div>
+
+      </div>
+    </section><!-- End Cta Section -->
+
+    <!-- ======= Portfolio Section ======= -->
+    <?php
+      // get all the images from the assets/img/portfolio/ folder
+      $images = glob('assets/img/portfolio/*.{jpg,jpeg,png,gif}', GLOB_BRACE);
+      // show them in the portfolio section
+      if (count($images) > 0) {
+        echo '<section id="portfolio" class="portfolio">
+          <div class="container" data-aos="fade-up">
+
+            <div class="section-title">
+              <h2>Photos</h2>
+              <p>Photos de notre activité</p>
+            </div>
+
+            <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">';
+        foreach ($images as $image) {
+          echo '<div class="col-lg-4 col-md-6 portfolio-item">
+            <div class="portfolio-wrap">
+              <img src="' . $image . '" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>' . basename($image) . '</h4>
+                <div class="portfolio-links">
+                  <a href="' . $image . '" data-gall="portfolioGallery" class="venobox" title="' . basename($image) . '"><i class="bx bx-plus"></i></a>
+                  <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>';
+        }
+        echo '</div>
+          </div>
+        </section>';
+      }
+    ?>
+    <section id="portfolio" class="portfolio">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Photos</h2>
+          <p>Photos de notre activité</p>
+        </div>
+
+        <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
+
+          <div class="col-lg-4 col-md-6 portfolio-item">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/climatiseurs.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>Climatiseurs</h4>
+                <div class="portfolio-links">
+                  <a href="assets/img/portfolio/climatiseurs.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Climatiseurs"><i class="bx bx-plus"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/carte de visite.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>Installation</h4>
+                <div class="portfolio-links">
+                  <a href="assets/img/portfolio/installation-1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Installation"><i class="bx bx-plus"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/installation-1.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>Installation</h4>
+                <div class="portfolio-links">
+                  <a href="assets/img/portfolio/installation-1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Installation"><i class="bx bx-plus"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/installation-4.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>Installation</h4>
+                <div class="portfolio-links">
+                  <a href="assets/img/portfolio/installation-4.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Installation"><i class="bx bx-plus"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/installation-2.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>Installation</h4>
+                <div class="portfolio-links">
+                  <a href="assets/img/portfolio/installation-2.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Installation"><i class="bx bx-plus"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/installation-3.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>Installation</h4>
+                <div class="portfolio-links">
+                  <a href="assets/img/portfolio/installation-3.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Installation"><i class="bx bx-plus"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/installation-5.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>Installation</h4>
+                <div class="portfolio-links">
+                  <a href="assets/img/portfolio/installation-5.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Installation"><i class="bx bx-plus"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+          <div class="col-lg-4 col-md-6 portfolio-item">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/installation-6.jpg" class="img-fluid" alt="">
+              <div class="portfolio-info">
+                <h4>Installation</h4>
+                <div class="portfolio-links">
+                  <a href="assets/img/portfolio/installation-6.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Installation"><i class="bx bx-plus"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+
+      </div>
+
+      </div>
+    </section><!-- End Portfolio Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Contact</h2>
+          <p>Contactez nous</p>
+        </div>
+        
+        <div class="row mt-5">
+          
+          <div class="col-lg-4">
+            <div class="info">
+              <div class="address">
+                <i class="bi bi-geo-alt"></i>
+                <h4>Adresse:</h4>
+                <p>19 Avenue Jean Baptiste Clément, 92100 Boulogne-Billancourt</p>
+              </div>
+
+              <div class="email">
+                <i class="bi bi-envelope"></i>
+                <h4>Email:</h4>
+                <p>clim.froid.idf@gmail.com</p>
+              </div>
+              
+              <div class="phone">
+                <i class="bi bi-phone"></i>
+                <h4>Téléphone:</h4>
+                <p>01 74 34 44 72</p>
+              </div>
+              
+              <div class="mapouter">
+                <div class="gmap_canvas">
+                  <iframe width="1080" height="316" id="gmap_canvas" src="https://maps.google.com/maps?q=19%20avenue%20jean%20baptiste%20cl%C3%A9ment&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                  <a href="https://www.whatismyip-address.com/divi-discount/">divi discount</a><br><style>.mapouter{position:relative;text-align:right;height:316px;width:1080px;}</style><a href="https://www.embedgooglemap.net">google map for websites</a>
+                  <style>.gmap_canvas {overflow:hidden;background:none!important;height:316px;width:1080px;}</style>
+                </div>
+              </div>
+              
+            </div>
+
+          </div>
+          
+          
+          <!-- <div class="col-lg-8 mt-5 mt-lg-0">
+
+            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+              <div class="row">
+                <div class="col-md-6 form-group">
+                  <input type="text" name="name" class="form-control" id="name" placeholder="Nom" required>
+                </div>
+                <div class="col-md-6 form-group mt-3 mt-md-0">
+                  <input type="email" class="form-control" name="email" id="email" placeholder="Email" required>
+                </div>
+              </div>
+              <div class="form-group mt-3">
+                <input type="text" class="form-control" name="subject" id="subject" placeholder="Objet" required>
+              </div>
+              <div class="form-group mt-3">
+                <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+              </div>
+              <div class="my-3">
+                <div class="loading">Loading</div>
+                <div class="error-message"></div>
+                <div class="sent-message">Votre message à été envoyer. Merci!</div>
+              </div>
+              <div class="text-center"><button type="submit">Envoyer</button></div>
+            </form> -->
+
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6">
+            <div class="footer-info">
+              <h3>SAS IDF Froid et Climatisation<span>.</span></h3>
+              <p>
+                19 Avenue Jean Baptiste Clément<br>
+                92100 Boulogne-Billancourt<br><br>
+                <strong>Téléphone:</strong> 01 74 34 44 72<br>
+                <strong>Email:</strong> clim.froid.idf@gmail.com<br>
+              </p>
+              <div class="social-links mt-3">
+                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6">
+            <div class="footer-info">
+              <h4>Informations</h4>
+              <p>
+                <strong>Numéro de siret :</strong> 898 872 791<br>
+                SAS au capital de 2000 euros<br>
+                Immatriculé au RCS de Nanterre<br>
+              </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>SAS IDF Froid et Climatisation</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/gp-free-multipurpose-html-bootstrap-template/ -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
