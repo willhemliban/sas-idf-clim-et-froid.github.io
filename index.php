0a237d23a3696d0f9158f02b84f6ab7d7cb38621
willhem <libanwillhem@gmail.com>
2023-01-17 15:31:13 +0100

    move portfolio folder for ease of access

--- index.php
+++ index.php
@@ -139,7 +139,7 @@
     <!-- ======= Portfolio Section ======= -->
     <?php
       // get all the images from the assets/img/portfolio/ folder
-      $images = glob('assets/img/portfolio/*.{jpg,jpeg,png,gif}', GLOB_BRACE);
+      $images = glob('photos/*.{jpg,jpeg,png,gif}', GLOB_BRACE);
       // show them in the portfolio section
       if (count($images) > 0) {
         echo '<section id="portfolio" class="portfolio">
@@ -170,119 +170,6 @@
         </section>';
       }
     ?>
-    <!-- <section id="portfolio" class="portfolio">
-      <div class="container" data-aos="fade-up">
-
-        <div class="section-title">
-          <h2>Photos</h2>
-          <p>Photos de notre activité</p>
-        </div>
-
-        <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
-
-          <div class="col-lg-4 col-md-6 portfolio-item">
-            <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/climatiseurs.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Climatiseurs</h4>
-                <div class="portfolio-links">
-                  <a href="assets/img/portfolio/climatiseurs.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Climatiseurs"><i class="bx bx-plus"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item">
-            <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/carte de visite.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Installation</h4>
-                <div class="portfolio-links">
-                  <a href="assets/img/portfolio/installation-1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Installation"><i class="bx bx-plus"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item">
-            <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/installation-1.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Installation</h4>
-                <div class="portfolio-links">
-                  <a href="assets/img/portfolio/installation-1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Installation"><i class="bx bx-plus"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item">
-            <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/installation-4.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Installation</h4>
-                <div class="portfolio-links">
-                  <a href="assets/img/portfolio/installation-4.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Installation"><i class="bx bx-plus"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item">
-            <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/installation-2.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Installation</h4>
-                <div class="portfolio-links">
-                  <a href="assets/img/portfolio/installation-2.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Installation"><i class="bx bx-plus"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item">
-            <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/installation-3.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Installation</h4>
-                <div class="portfolio-links">
-                  <a href="assets/img/portfolio/installation-3.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Installation"><i class="bx bx-plus"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 portfolio-item">
-            <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/installation-5.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Installation</h4>
-                <div class="portfolio-links">
-                  <a href="assets/img/portfolio/installation-5.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Installation"><i class="bx bx-plus"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-          
-          <div class="col-lg-4 col-md-6 portfolio-item">
-            <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/installation-6.jpg" class="img-fluid" alt="">
-              <div class="portfolio-info">
-                <h4>Installation</h4>
-                <div class="portfolio-links">
-                  <a href="assets/img/portfolio/installation-6.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Installation"><i class="bx bx-plus"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-          
-        </div>
-      </div>
-
-      </div>
-
-      </div>
-    </section>End Portfolio Section -->
 
     <!-- ======= Contact Section ======= -->
     <section id="contact" class="contact">
